8c16f48a7e9911680ef644d0792c08a5ea019d35
Craig Martin <craig.martin@accedo.tv>
2025-03-26 17:07:09 +1100

    project

---
+++ AMPMobileChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMPMobileChallenge
+//
+//  Created by Craig Martin on 26/3/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
